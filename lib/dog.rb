a0d606f072907ebb873f49114d7c7701dc5192a7
Peter Lin <peterlin5301997@gmail.com>
2019-06-26 01:21:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
 # Add your code here
+class Dog
+  @@all = []
+
+  def initialize(dogs_name)
+    @name = dogs_name
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
