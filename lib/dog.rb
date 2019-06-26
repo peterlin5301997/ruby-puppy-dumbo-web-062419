7812f80be1976a5d7a6074d3f916bee42adcce15
Peter Lin <peterlin5301997@gmail.com>
2019-06-26 01:13:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,7 +4,7 @@ class Dog
 
   def initialize(dogs_name)
     @name = dogs_name
-    @@all << self
+    @all << self
   end
 
   def self.all
@@ -12,7 +12,7 @@ class Dog
   end
 
   def self.print_all
-    @@all.each do |dog|
+    @all.each do |dog|
       puts @name
     end
   end
