0543ec3948bb766bc10e89c92e31887b2467c61a
Peter Lin <peterlin5301997@gmail.com>
2019-06-26 01:11:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Add your code here
+class Dog
+  @@all = []
+
+  def initialize(dogs_name)
+    @name = dogs_name
+    @@all << self
+  end
+
+  def .all
+    @@all
+  end
+
+  def .print_all
+    @@all.each do |dog|
+      puts dog
+    end
+  end
+end
